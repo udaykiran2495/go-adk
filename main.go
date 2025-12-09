6f72c0c49a28f92742c8f80599a3735db26b6b57
Uday Kiran <udaykiran2495@gmail.com>
2025-12-10 00:41:13 +0530

    weather agent

--- main.go
+++ main.go
@@ -1,7 +1,131 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"net/url"
+	"os"
+
+	"github.com/a2aproject/a2a-go/a2a"
+	"github.com/a2aproject/a2a-go/a2asrv"
+
+	"google.golang.org/genai"
+
+	"github.com/joho/godotenv"
+
+	"google.golang.org/adk/agent"
+	"google.golang.org/adk/agent/llmagent"
+	"google.golang.org/adk/agent/remoteagent"
+	"google.golang.org/adk/cmd/launcher"
+	"google.golang.org/adk/cmd/launcher/full"
+	"google.golang.org/adk/model/gemini"
+	"google.golang.org/adk/runner"
+	"google.golang.org/adk/server/adka2a"
+	"google.golang.org/adk/session"
+	"google.golang.org/adk/tool"
+	"google.golang.org/adk/tool/geminitool"
+)
+
+const MODEL_NAME = "gemini-2.5-flash"
+
+func newWeatherAgent(ctx context.Context) agent.Agent {
+	model, err := gemini.NewModel(ctx, MODEL_NAME, &genai.ClientConfig{
+		APIKey: os.Getenv("GOOGLE_API_KEY"),
+	})
+	if err != nil {
+		log.Fatalf("Failed to create model: %v", err)
+	}
+
+	agent, err := llmagent.New(llmagent.Config{
+		Name:        "weather_time_agent",
+		Model:       model,
+		Description: "Agent to answer questions about the time and weather in a city.",
+		Instruction: "I can answer your questions about the time and weather in a city.",
+		Tools:       []tool.Tool{geminitool.GoogleSearch{}},
+	})
+
+	if err != nil {
+		log.Fatalf("Failed to create agent: %v", err)
+	}
+
+	return agent
+}
+
+func startWeatherAgentServer() string {
+	listener, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		log.Fatalf("Failed to bind to a port: %v", err)
+	}
+
+	baseURL := &url.URL{Scheme: "http", Host: listener.Addr().String()}
+
+	log.Printf("Starting A2A server on %s", baseURL.String())
+
+	go func() {
+		ctx := context.Background()
+		agent := newWeatherAgent(ctx)
+
+		agentPath := "/invoke"
+		agentCard := &a2a.AgentCard{
+			Name:               agent.Name(),
+			Skills:             adka2a.BuildAgentSkills(agent),
+			PreferredTransport: a2a.TransportProtocolJSONRPC,
+			URL:                baseURL.JoinPath(agentPath).String(),
+			Capabilities:       a2a.AgentCapabilities{Streaming: true},
+		}
+
+		mux := http.NewServeMux()
+		mux.Handle(a2asrv.WellKnownAgentCardPath, a2asrv.NewStaticAgentCardHandler(agentCard))
+
+		executor := adka2a.NewExecutor(adka2a.ExecutorConfig{
+			RunnerConfig: runner.Config{
+				AppName:        agent.Name(),
+				Agent:          agent,
+				SessionService: session.InMemoryService(),
+			},
+		})
+		requestHandler := a2asrv.NewHandler(executor)
+		mux.Handle(agentPath, a2asrv.NewJSONRPCHandler(requestHandler))
+
+		err := http.Serve(listener, mux)
+
+		log.Printf("A2A server stopped: %v", err)
+	}()
+
+	return baseURL.String()
+
+}
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found, relying on system environment variables")
+	}
+
 	fmt.Println("Hello, World!")
+
+	ctx := context.Background()
+
+	a2aServerAddress := startWeatherAgentServer()
+
+	remoteAgent, err := remoteagent.NewA2A(remoteagent.A2AConfig{
+		Name:            "A2A Weather agent",
+		AgentCardSource: a2aServerAddress,
+	})
+
+	if err != nil {
+		log.Fatalf("Failed to create remote agent: %v", err)
+	}
+
+	config := &launcher.Config{
+		AgentLoader: agent.NewSingleLoader(remoteAgent),
+	}
+
+	l := full.NewLauncher()
+
+	if err = l.Execute(ctx, config, os.Args[1:]); err != nil {
+		log.Fatalf("Launcher execution failed: %v", err)
+	}
 }
